d1ac71d17de6169577134bfde24a92cee7c23389
igorl <igorllampa@gmail.com>
2023-09-05 21:23:31 -0300

    add persistence of photo archive information at database and service to substitute photo

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/RestauranteProdutoFotoController.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,38 +1,67 @@
 package com.lampasw.algafood.api.controller;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.UUID;
-
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.lampasw.algafood.api.assembler.FotoProdutoModelAssembler;
+import com.lampasw.algafood.api.model.FotoProdutoModel;
 import com.lampasw.algafood.api.model.input.FotoProdutoInput;
+import com.lampasw.algafood.domain.model.FotoProduto;
+import com.lampasw.algafood.domain.model.Produto;
+import com.lampasw.algafood.domain.service.CadastroProdutoService;
+import com.lampasw.algafood.domain.service.CatalogoFotoProdutoService;
 
 @RestController
 @RequestMapping("restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
 
+	@Autowired
+	private CatalogoFotoProdutoService catalogoFotoProduto;
+	
+	@Autowired
+	private CadastroProdutoService cadastroProduto;
+	
+	@Autowired
+	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
+	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFoto(@PathVariable Long restauranteId, 
+	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, 
 			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
 		
-		var nomeArquivo = UUID.randomUUID().toString() + 
-				"_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+		Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
 		
-		var arquivoFoto = Path.of("C:\\temp\\algaworks-foto-produto", nomeArquivo);
+		MultipartFile arquivo = fotoProdutoInput.getArquivo();
+		
+		FotoProduto foto = new FotoProduto();
+		foto.setProduto(produto);
+		foto.setDescricao(fotoProdutoInput.getDescricao());
+		foto.setContentType(arquivo.getContentType());
+		foto.setNomeArquivo(arquivo.getOriginalFilename());
+		foto.setTamanho(arquivo.getSize());
 		
 		
-		try {
-			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
-		} catch (IOException e) {		
-			e.printStackTrace();
-		}
+		FotoProduto fotoSalva = catalogoFotoProduto.salvarFoto(foto);
+		
+		return fotoProdutoModelAssembler.toModel(fotoSalva);
+		
+//		var nomeArquivo = UUID.randomUUID().toString() + 
+//				"_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+//		
+//		var arquivoFoto = Path.of("C:\\temp\\algaworks-foto-produto", nomeArquivo);
+//		
+//		
+//		try {
+//			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+//		} catch (IOException e) {		
+//			e.printStackTrace();
+//		}
 		
 		
 	}
