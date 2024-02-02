1a5dcabf53bd47b74036c9b3b1e88c874b577b29
igorl <igorllampa@gmail.com>
2024-02-02 17:51:41 -0300

    Add method to get the image file at jpeg format file

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/RestauranteProdutoFotoController.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,12 +1,15 @@
 package com.lampasw.algafood.api.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,11 +20,13 @@ import org.springframework.web.multipart.MultipartFile;
 import com.lampasw.algafood.api.assembler.FotoProdutoModelAssembler;
 import com.lampasw.algafood.api.model.FotoProdutoModel;
 import com.lampasw.algafood.api.model.input.FotoProdutoInput;
+import com.lampasw.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.lampasw.algafood.domain.model.FotoProduto;
 import com.lampasw.algafood.domain.model.Produto;
 import com.lampasw.algafood.domain.repository.ProdutoRepository;
 import com.lampasw.algafood.domain.service.CadastroProdutoService;
 import com.lampasw.algafood.domain.service.CatalogoFotoProdutoService;
+import com.lampasw.algafood.domain.service.FotoStorageService;
 
 @RestController
 @RequestMapping("restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -31,16 +36,37 @@ public class RestauranteProdutoFotoController {
 	private CatalogoFotoProdutoService catalogoFotoProduto;
 	
 	@Autowired
+	private FotoStorageService fotoStorage;
+	
+	@Autowired
 	private CadastroProdutoService cadastroProduto;
 	
 	@Autowired
 	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public FotoProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
 		FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
 		
 		return fotoProdutoModelAssembler.toModel(fotoProduto);
+	}
+	
+	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId){
+		try {
+			
+		
+		FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+		
+		InputStream arquivo = fotoStorage.recuperar(fotoProduto.getNomeArquivo());
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.IMAGE_JPEG)
+				.body(new InputStreamResource(arquivo));
+		}catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		}		
+		
 	}
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
