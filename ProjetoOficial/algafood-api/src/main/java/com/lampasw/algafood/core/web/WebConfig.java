9a4febcf198a8d3282f383b72577a677ebc13f7f
igorl <igorllampa@gmail.com>
2024-02-21 22:54:07 -0300

    Add Shallow ETags to all responses of the API, to reduce unnecessary traffic

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/core/web/WebConfig.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package com.lampasw.algafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
 			//.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
 			//.maxAge(30)
 			
+	}
+	
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
