9040be7acddb31eb7892eebd7f323d21dbeee2fb
igorl <igorllampa@gmail.com>
2024-05-28 14:31:53 -0300

    Comment some code used to just understand and practise de options

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/CidadeModel.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/CidadeModel.java
@@ -1,20 +1,19 @@
 package com.lampasw.algafood.api.model;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-@ApiModel(value = "Cidade", description = "Representa uma cidade")
+//@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModel {
 
 	@ApiModelProperty(value = "ID da cidade", example = "1")
-	private Long id;	
-	
+	private Long id;
+
 	@ApiModelProperty(example = "Monte Alto")
-	private String nome;		
-	
-	private EstadoModel estado;	
+	private String nome;
+
+	private EstadoModel estado;
 }
