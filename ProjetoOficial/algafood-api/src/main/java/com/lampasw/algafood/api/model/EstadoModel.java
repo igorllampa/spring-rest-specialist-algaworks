39e22ef1fe8f5886f00697e1dcd17bab10047b85
igorl <igorllampa@gmail.com>
2024-05-28 14:29:43 -0300

    Add properties description and example, to improve our documentation

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/EstadoModel.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/EstadoModel.java
@@ -1,12 +1,16 @@
 package com.lampasw.algafood.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EstadoModel {
-	
+
+	@ApiModelProperty(example = "1")
 	private Long id;	
+	
+	@ApiModelProperty(example = "São Paulo")
 	private String nome;
 }
