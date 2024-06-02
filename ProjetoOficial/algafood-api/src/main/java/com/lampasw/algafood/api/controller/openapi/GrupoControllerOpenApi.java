db56af4cea24955d9ea66c76632ee8006c3811d0
igorl <igorllampa@gmail.com>
2024-06-02 14:48:39 -0300

    Add openApi documentation tags for SpringFox at groups controller interface

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/openapi/GrupoControllerOpenApi.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/openapi/GrupoControllerOpenApi.java
@@ -2,21 +2,48 @@ package com.lampasw.algafood.api.controller.openapi;
 
 import java.util.List;
 
+import com.lampasw.algafood.api.exceptionhandler.Problem;
 import com.lampasw.algafood.api.model.GrupoModel;
 import com.lampasw.algafood.api.model.input.GrupoInput;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "Grupos")
 public interface GrupoControllerOpenApi {
 
+	@ApiOperation("Lista os grupos")
 	public List<GrupoModel> listar();
 	
-	public GrupoModel buscar(Long grupoId);
+	@ApiOperation("Busca um grupo por Id")
+	@ApiResponses({
+		@ApiResponse(code = 400, message = "ID do grupo inválido", response = Problem.class),
+		@ApiResponse(code = 404, message = "Grupo não encontrada", response = Problem.class)
+	})
+	public GrupoModel buscar(@ApiParam(value = "ID de um grupo", example = "0") Long grupoId);
 	
-	public GrupoModel adicionar(GrupoInput grupoInput);
+	@ApiOperation("Adiciona um novo grupo")
+	@ApiResponses({
+		@ApiResponse(code = 201, message = "Grupo cadastrado")
+	})
+	public GrupoModel adicionar(@ApiParam(name="corpo", value = "Representação de um novo grupo") GrupoInput grupoInput);
 	
-	public GrupoModel atualizar(Long grupoId, GrupoInput grupoInput);
+	@ApiOperation("Atualiza um grupo por Id")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Grupo atualizada"),
+		@ApiResponse(code = 404, message = "Grupo não encontrada", response = Problem.class)
+	})
+	public GrupoModel atualizar(
+			@ApiParam(value = "ID de um grupo", example = "0") Long grupoId, 
+			@ApiParam(name = "corpo", value = "Representação de um grupo com os novos dados") GrupoInput grupoInput);
 	
+	@ApiOperation("Remove um grupo por Id")
+	@ApiResponses({
+		@ApiResponse(code = 204, message = "Grupo excluído"),
+		@ApiResponse(code = 404, message = "Grupo não encontrado", response = Problem.class)
+	})
 	public void remover(Long grupoId);	
 }
