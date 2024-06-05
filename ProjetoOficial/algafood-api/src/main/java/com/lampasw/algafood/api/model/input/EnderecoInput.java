1567bbf1148fb8b213fa7a56482dd273e98b7e5a
igorl <igorllampa@gmail.com>
2024-06-05 20:36:43 -0300

    Add documentation for inputs used atPedidoController

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/input/EnderecoInput.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/input/EnderecoInput.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,21 +12,26 @@ import lombok.Setter;
 @Setter
 public class EnderecoInput {
 
+	@ApiModelProperty(example = "15555-000", required = true)
 	@NotBlank
 	private String cep;
 	
+	@ApiModelProperty(example = "Av. Paulista", required = true)
 	@NotBlank
 	private String logradouro;
 	
+	@ApiModelProperty(example = "123", required = true)
 	@NotBlank
 	private String numero;
 	
-	
+	@ApiModelProperty(example = "Bloco C")
 	private String complemento;
-	
+		
+	@ApiModelProperty(example = "Barra Funda", required = true)
 	@NotBlank
 	private String bairro;
 	
+	@ApiModelProperty(example = "São Paulo", required = true)
 	@Valid
 	@NotNull
 	private CidadeIdInput cidade;	
--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/input/FormaDePagamentoIdInput.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/input/FormaDePagamentoIdInput.java
@@ -2,6 +2,7 @@ package com.lampasw.algafood.api.model.input;
 
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 public class FormaDePagamentoIdInput {
 
+	@ApiModelProperty(example = "0", required = true)
 	@NotNull
 	private Long id;
 }
