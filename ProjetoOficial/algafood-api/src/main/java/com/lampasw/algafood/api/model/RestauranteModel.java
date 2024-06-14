766ddf3fc6da2ba0e7f24fddb86b05bca498810c
igorl <igorllampa@gmail.com>
2024-06-13 22:48:19 -0300

    Add documentation for parameters that is used at input and model related to Restaurant input and output

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/RestauranteModel.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/model/RestauranteModel.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.lampasw.algafood.api.model.view.RestauranteView;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,19 +13,26 @@ import lombok.Setter;
 @Setter
 public class RestauranteModel {
 
+	@ApiModelProperty(example = "0")
 	@JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})	
 	private Long id;
 	
+	@ApiModelProperty(example = "Monte Alto Food")
 	@JsonView({RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})	
 	private String nome;
 	
+	@ApiModelProperty(example = "5.80")
 	@JsonView(RestauranteView.Resumo.class)
 	private BigDecimal taxaFrete;
 	
 	@JsonView(RestauranteView.Resumo.class)
 	private CozinhaModel cozinha;
 	
+	@ApiModelProperty(example = "true")
 	private Boolean ativo;
+	
+	@ApiModelProperty(example = "true")
 	private Boolean aberto;
+	
 	private EnderecoModel endereco;
 }
