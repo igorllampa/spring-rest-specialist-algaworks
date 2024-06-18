31143f75d6000f0dad9643605fa8a8530ae18ff8
igorl <igorllampa@gmail.com>
2024-06-18 11:54:51 -0300

    Add documentation for RestauranteFormaDePagamentoController

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/RestauranteFormaDePagamentoController.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/RestauranteFormaDePagamentoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lampasw.algafood.api.assembler.FormaDePagamentoModelAssembler;
 import com.lampasw.algafood.api.model.FormaDePagamentoModel;
+import com.lampasw.algafood.api.openapi.controller.RestauranteFormaDePagamentoControllerOpenApi;
 import com.lampasw.algafood.domain.model.Restaurante;
 import com.lampasw.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
-@RequestMapping("restaurantes/{restauranteId}/formas-de-pagamento")
-public class RestauranteFormaDePagamentoController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/formas-de-pagamento", 
+	produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteFormaDePagamentoController implements RestauranteFormaDePagamentoControllerOpenApi {
 	
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
---
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/openapi/controller/RestauranteFormaDePagamentoControllerOpenApi.java
@@ -0,0 +1,42 @@
+package com.lampasw.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.lampasw.algafood.api.exceptionhandler.Problem;
+import com.lampasw.algafood.api.model.FormaDePagamentoModel;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteFormaDePagamentoControllerOpenApi {
+
+	@ApiOperation("Lista as formas de pagamento associadas a restaurante")
+    @ApiResponses({
+        @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
+    })
+	public List<FormaDePagamentoModel> listar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId);
+
+	@ApiOperation("Associação de restaurante com forma de pagamento")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+        @ApiResponse(code = 404, message = "Restaurante ou forma de pagamento não encontrado", 
+            response = Problem.class)
+    })
+	public void associar(
+			@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@ApiParam(value = "ID da forma de pagamento", example = "1", required = true) Long formaDePagamentoId);
+	
+	 @ApiOperation("Desassociação de restaurante com forma de pagamento")
+	    @ApiResponses({
+	        @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+	        @ApiResponse(code = 404, message = "Restaurante ou forma de pagamento não encontrado", 
+	            response = Problem.class)
+	    })
+	public void desassociar(
+			@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@ApiParam(value = "ID da forma de pagamento", example = "1", required = true) Long formaDePagamentoId);
+}
