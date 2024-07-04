72762fe11e6b6326e433201ee27087b51cac661d
igorl <igorllampa@gmail.com>
2024-07-03 22:39:32 -0300

    Add dynamic links to implement hateoas at endpoint of city find

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/CidadeController.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/CidadeController.java
@@ -1,12 +1,12 @@
 package com.lampasw.algafood.api.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.IanaLinkRelations;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,76 +39,79 @@ public class CidadeController implements CidadeControllerOpenApi {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private CadastroCidadeService cadastroCidade;
-	
+
 	@Autowired
 	private CidadeModelAssembler cidadeModelAssembler;
-	
+
 	@Autowired
 	private CidadeInputDisassembler cidadeInputDisassembler;
-	
+
 	@GetMapping
-	public List<CidadeModel> listar(){
-		List<Cidade> cidades = cidadeRepository.findAll(); 
-		
+	public List<CidadeModel> listar() {
+		List<Cidade> cidades = cidadeRepository.findAll();
+
 		return cidadeModelAssembler.toCollectionModel(cidades);
 	}
-		
+
 	@GetMapping("/{cidadeId}")
-	public CidadeModel buscar(@PathVariable Long cidadeId){
-		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId); 
-		
+	public CidadeModel buscar(@PathVariable Long cidadeId) {
+		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
+
 		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
-		
-		cidadeModel.add(Link.of("localhost:8080/cidades/1", IanaLinkRelations.SELF));
-		cidadeModel.add(Link.of("http://api.algafood.local:8080/cidades", IanaLinkRelations.COLLECTION));		
-		cidadeModel.getEstado().add(Link.of("http://api.algafood.local:8080/estados/1"));
-		
+
+		// cidadeModel.add(Link.of("localhost:8080/cidades/1", IanaLinkRelations.SELF));
+		cidadeModel.add(linkTo(CidadeController.class).slash(cidadeModel.getId()).withSelfRel());
+
+		// cidadeModel.add(Link.of("http://api.algafood.local:8080/cidades",
+		// IanaLinkRelations.COLLECTION));
+		cidadeModel.add(linkTo(CidadeController.class).withRel("cidades"));
+
+		// cidadeModel.getEstado().add(Link.of("http://api.algafood.local:8080/estados/1"));
+		cidadeModel.getEstado()
+				.add(linkTo(EstadoController.class).slash(cidadeModel.getEstado().getId()).withSelfRel());
+
 		return cidadeModel;
 	}
-		
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public CidadeModel adicionar(
-			@RequestBody @Valid CidadeInput cidadeInput){		
+	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
 			cidade = cadastroCidade.salvar(cidade);
-			
+
 			ResourceUriHelper.addUriInReponseHeader(cidade.getId());
-			
-			return cidadeModelAssembler.toModel(cidade);	
-		}catch (EstadoNaoEncontradoException e) {
+
+			return cidadeModelAssembler.toModel(cidade);
+		} catch (EstadoNaoEncontradoException e) {
 			throw new NegocioException(e.getMessage(), e);
 		}
 	}
-		
+
 	@PutMapping("/{cidadeId}")
-	public CidadeModel atualizar(			
-			@PathVariable Long cidadeId,			
-			@RequestBody @Valid CidadeInput cidadeInput){
-					
+	public CidadeModel atualizar(@PathVariable Long cidadeId, @RequestBody @Valid CidadeInput cidadeInput) {
+
 		Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
-		
-					
-		try {								
+
+		try {
 			cidadeInputDisassembler.copyToDomainObject(cidadeInput, cidadeAtual);
-			
+
 			cidadeAtual = cadastroCidade.salvar(cidadeAtual);
-			
+
 			return cidadeModelAssembler.toModel(cidadeAtual);
-			
-		}catch (EstadoNaoEncontradoException e) {
+
+		} catch (EstadoNaoEncontradoException e) {
 			throw new NegocioException(e.getMessage(), e);
 		}
 	}
-		
+
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
-	public void remover(@ApiParam(value = "ID de uma cidade", example = "0") @PathVariable Long cidadeId){		
-		cadastroCidade.remover(cidadeId);		
-	}	
-	
+	public void remover(@ApiParam(value = "ID de uma cidade", example = "0") @PathVariable Long cidadeId) {
+		cadastroCidade.remover(cidadeId);
+	}
+
 }
