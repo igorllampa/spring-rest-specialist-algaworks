67fef33b134e1a5beca5e4482af4b2429a693806
igorl <igorllampa@gmail.com>
2024-07-04 07:48:59 -0300

    Implementing hateoas with spring methods to avoid hardcoded or manually url build

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/CidadeController.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/CidadeController.java
@@ -1,6 +1,7 @@
 package com.lampasw.algafood.api.controller;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
 
@@ -61,11 +62,10 @@ public class CidadeController implements CidadeControllerOpenApi {
 		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
 
 		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
-		
-		cidadeModel.add(linkTo(CidadeController.class).slash(cidadeModel.getId()).withSelfRel());
-		cidadeModel.add(linkTo(CidadeController.class).withRel("cidades"));	
-		cidadeModel.getEstado()
-				.add(linkTo(EstadoController.class).slash(cidadeModel.getEstado().getId()).withSelfRel());
+
+		cidadeModel.add(linkTo(methodOn(CidadeController.class).buscar(cidadeModel.getId())).withSelfRel());
+		cidadeModel.add(linkTo(methodOn(CidadeController.class).listar()).withRel("cidades"));
+		cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class).buscar(cidadeModel.getEstado().getId())).withSelfRel());
 
 		return cidadeModel;
 	}
