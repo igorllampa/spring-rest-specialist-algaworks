ead2186d633d9f5e319e31e3e27fbe28da2fb06b
igorl <igorllampa@gmail.com>
2024-07-04 22:31:35 -0300

    Add hateoas for pagination at endpoint used to liss cozinhas

--- ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/CozinhaController.java
+++ ProjetoOficial/algafood-api/src/main/java/com/lampasw/algafood/api/controller/CozinhaController.java
@@ -9,6 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -48,19 +51,21 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	@Autowired
 	private CozinhaInputDisassembler cozinhaInputDisassembler;
 	
+	@Autowired
+	private PagedResourcesAssembler<Cozinha> pagedResourcesAssembler;
+	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable){
+	public PagedModel<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable){
 		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
 		
-		List<CozinhaModel> cozinhasModel = cozinhaModelAssembler.toCollectionModel(cozinhasPage.getContent());
+		PagedModel<CozinhaModel> cozinhasPagedModel = pagedResourcesAssembler
+				.toModel(cozinhasPage, cozinhaModelAssembler);
 		
-		Page<CozinhaModel> cozinhasModelPage = new PageImpl<>(cozinhasModel, pageable, cozinhasPage.getTotalElements());
-		
-		return cozinhasModelPage;
+		return cozinhasPagedModel;
 	}
 	
 	@GetMapping("/listar-por-nome")
-	public List<CozinhaModel> buscarPorNome(@RequestParam String nome){
+	public CollectionModel<CozinhaModel> buscarPorNome(@RequestParam String nome){
 		List<Cozinha> cozinhas = cozinhaRepository.findByNomeContains(nome);
 		
 		return cozinhaModelAssembler.toCollectionModel(cozinhas);
